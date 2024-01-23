14d2ed18f828f748f6c9c13752adacf96d9db913
SebastianSamboni <sebas_18_08@hotmail.com>
2024-01-23 10:47:34 -0500

    Add changing menu on NavBar

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react"
+import React, { useEffect, useContext } from "react"
 import { ProductCard } from "../graphic- interface/components/ProductCard"
 import { ProductContext } from "../context/ProductContext"
 
