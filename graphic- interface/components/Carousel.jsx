6139ef9509696f0abae4f2e8fe4b2d3c55c85bdf
Camilo Avarado <camealvarado5253@gmail.com>
2024-01-22 21:48:04 -0500

    feat: fix carousel

--- graphic- interface/components/Carousel.jsx
+++ graphic- interface/components/Carousel.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { img1 } from '../../assets/carouselImages/carrusel1.png'
+import { img2 } from '../../assets/carouselImages/carrusel2.jpg'
+import { img3 } from '../../assets/carouselImages/carrusel3.png'
+import { img4 } from '../../assets/carouselImages/carrusel4.jpg'
+import { img5 } from '../../assets/carouselImages/carrusel5.jpg'
 
 export const Carousel = () => {
   const settings = {
@@ -17,19 +22,19 @@ export const Carousel = () => {
   return (
     <Slider {...settings}>
       <div className="col-md-10 d-flex justify-content-center" >
-        <img src="../../assets/carouselImages/carrusel1.png" alt="img 1" className='img-carousel'/>
+        <img src={ img1 } alt="img 1" className='img-carousel'/>
       </div>
       <div>
-        <img src="../../assets/carouselImages/carrusel2.jpg" alt="img 2" className='img-carousel'/>
+        <img src={ img2 } alt="img 2" className='img-carousel'/>
       </div>
       <div>
-        <img src="../../assets/carouselImages/carrusel3.png" alt="img 3" className='img-carousel'/>
+        <img src={ img3 } alt="img 3" className='img-carousel'/>
       </div>
       <div>
-        <img src="../../assets/carouselImages/carrusel4.jpg" alt="img 4" className='img-carousel'/>
+        <img src={ img4 } alt="img 4" className='img-carousel'/>
       </div>
       <div>
-        <img src="../../assets/carouselImages/carrusel5.jpg" alt="img 5" className='img-carousel'/>
+        <img src={ img5 } alt="img 5" className='img-carousel'/>
       </div>
     </Slider>
   );
